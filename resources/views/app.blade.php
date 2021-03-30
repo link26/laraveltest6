c8e8d641a37ec1b45d6fdbe55fb17526adf2f70e
link26 <link26@list.ru>
2021-03-30 20:37:54 +0300

    osn

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!-- resources/views/layouts/app.blade.php -->
+<!--osn shablon index.php-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('title')</title>
+
+    <!-- CSS и JavaScript -->
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Содержимое Navbar -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+Route::get('/link', function () {
+    return view('link');
 });
